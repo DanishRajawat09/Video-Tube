9d9c8510a7fc854a8fad56161a42089a578a0948
Danish Rajawat <ibnfarooq070@gmail.com>
2025-02-10 16:02:15 +0530

    creating custom api response and error handling and create wrapper for database

--- src/index.js
+++ src/index.js
@@ -1,31 +1,40 @@
 // require('dotenv').config({path : './env'})
 
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 
 dotenv.config({
-    path : "./env"
-})
+  path: "./env",
+});
 
-connectDB()
-
-
-
-
-
-
-
-
-
-
-
-
-
+connectDB().then((params) => {
+    app.on("error", (error) => {
+      console.log("err : ", error);
+      process.exit(1);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at Port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB connection failed !! ", err);
+  });
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+  
 // import express from "express";
 // const app = express()(async () => {
 //   try {
